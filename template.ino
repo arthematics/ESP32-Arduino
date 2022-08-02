8c4f48a096a851545250e48f71e4fd6d067bec97
Artem Pokintelitsa <11274496+arthematics@users.noreply.github.com>
2022-08-02 15:27:11 +0300

    Create template.ino

---
+++ template.ino
@@ -0,0 +1,55 @@
+//------10 -------20 -------30 -------40 -------50 -------60 -------70 -------80
+
+/*********
+  Author
+  
+  Name:
+  Project name
+  
+  Description:
+  A brief description.
+  
+  Notes:
+  Some notes.
+*********/
+
+/*
+  DEFINED CONSTANTS AND SETTINGS
+*/
+#define CONSTANT_NAME value
+
+const TYPE name = value;
+
+/*
+  LIBRARIES AND FILES
+*/
+#include <Servo.h>
+Servo myservo;
+
+/*
+  VARIABLES
+*/
+uint32_t servoTimer;
+boolean servoDirection;
+int servoAngle;
+
+/*
+  SETUP FUNCTION
+*/
+void setup() {
+
+}
+
+/*
+  LOOP FUNCTION
+*/
+void loop() {
+
+}
+
+/*
+  FUNCTIONS
+*/
+void makeSomething() {
+
+}
